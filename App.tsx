143792d39c559c9cc26a90e16cd042ac81957286
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-05-19 14:57:33 -0300

    Configurando Fontes

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
+import {ActivityIndicator} from 'react-native';
+import {ThemeProvider} from 'styled-components'
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
 
+import theme from  './src/theme'
+
 export default function App() {
+
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+    </ThemeProvider>
   );
 }
 
