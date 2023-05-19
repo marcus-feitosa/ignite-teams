b7a3bc0fc6757b033e21ad2ff90cd16cf5f3f08a
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-05-19 15:08:42 -0300

    Configurando Status Bar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-
+import { StatusBar } from 'react-native';
 import {ThemeProvider} from 'styled-components'
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
@@ -13,6 +13,12 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
