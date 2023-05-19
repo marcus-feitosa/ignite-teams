4cca4561d7ac11cfbf7f5626cb0957bf4e5d4abf
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-05-19 15:05:35 -0300

    Criando componente Loading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import {ActivityIndicator} from 'react-native';
+
 import {ThemeProvider} from 'styled-components'
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 import { Groups } from '@screens/Groups'
+
+import { Loading } from '@components/Loading';
 
 import theme from  './src/theme'
 
@@ -11,7 +13,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
