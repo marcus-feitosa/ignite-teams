10a1c62d74aa86e675bf27b2fe490187cc8d59f5
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-05-29 16:25:11 -0300

    Criando Header

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
+import { Header } from '@components/Header';
 import { StyleSheet, Text } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
   return (
     <Container>
-      <Text>Groups</Text>
+
+      <Header />
+      
     </Container>
   );
 }
