311c15f4a4827bc32ff58403ef087e9e0cc401c2
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-12 16:48:16 -0300

    Interface Players

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'react-native';
 import {ThemeProvider} from 'styled-components'
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
-import { NewGroup } from '@screens/NewGroup'
+import { Players } from '@screens/Players'
 
 import { Loading } from '@components/Loading';
 
@@ -19,7 +19,7 @@ export default function App() {
         translucent
 
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
