61a0d5995303d52918a5e711408ff5e7046dac6d
marcus-feitosa <marcusfeitosa02@gmail.com>
2023-06-14 16:03:53 -0300

    Criando icon Button

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import {Container} from './styles' 
+import { ButtonIcon } from "@components/ButtonIcon";
 
 export function Players(){
     return(
         <Container>
-            <Header showBackButton/>
-            <Highlight title='NOme da turma' subtitle='Adicione a galera e separe os times'/>
+            <Header showBackButton/>   
+            <Highlight title='Nome da turma' subtitle='Adicione a galera e separe os times'/>
+            <ButtonIcon />
         </Container>
     )
 }
